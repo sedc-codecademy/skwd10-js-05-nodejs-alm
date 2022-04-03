46526829c67e485f8b9a5f9e2ace84ab74603ed5
notokkid <Ivan.Lazarevski@seavus.com>
2022-04-03 20:50:34 +0200

    Added comments to the code from class

--- G9/Class_09-Cookies-Sessions/API/const/session.const.js
+++ G9/Class_09-Cookies-Sessions/API/const/session.const.js
@@ -1,7 +1,7 @@
 const session = require("express-session");
 
 module.exports = session({
-  secret: ["asdfghjkl"], // To update the secret add a new value,
+  secret: ["asdfghjkl"], // To update the secret add a new value, dont delete the previous.
   name: "cookie_id",
   cookie: {
     maxAge: 5 * 60 * 60 * 1000,
@@ -11,3 +11,38 @@ module.exports = session({
   saveUninitialized: true,
   resave: false
 });
+
+/*
+[SESSION SECRET]
+But because these cookies are stored by the web browser (the client), 
+the web server doesn't actually know that the cookies it receives from the client are legitimate.
+
+A session secret in connect is simply used to compute the hash. 
+Without the string, access to the session would essentially be "denied".
+The session secret is a key used for signing and/or encrypting cookies set by the application to maintain session state.
+
+
+Let's assume that sessions are enabled globally (for all requests).
+When a client makes an HTTP request, and that request doesn't contain a session cookie, 
+a new session will be created by express-session.
+
+If during the lifetime of the request the session object isn't modified then, 
+at the end of the request and when saveUninitialized is false, the (still empty, because unmodified) session object 
+will not be stored in the session store.
+
+The reasoning behind this is that this will prevent a lot of empty session objects being stored in the session store. 
+Since there's nothing useful to store, the session is "forgotten" at the end of the request.
+
+When do you want to enable this? When you want to be able to identify recurring visitors, for example. 
+You'd be able to recognize such a visitor because they send the session cookie containing the unique id.
+
+About resave: this may have to be enabled for session stores that don't support the "touch" command. 
+What this does is tell the session store that a particular session is still active, 
+which is necessary because some stores will delete idle (unused) sessions after some time.
+
+If a session store driver doesn't implement the touch command, 
+then you should enable resave so that even when a session wasn't changed during a request, 
+it is still updated in the store (thereby marking it active).
+
+So it entirely depends on the session store that you're using if you need to enable this option or not.
+*/
---
+++ G9/Class_10-workshop/Class_10-workshop-requirements.md
@@ -0,0 +1,9 @@
+Create an MVC ExpressJS application for a Rent-a-car business. 
+
+1. The API needs to have a route to fetch all cars from the database 
+2. The API needs to have a route to fetch a single car by ID from the database 
+3. The API needs to have a route to create new cars in the database 
+4. The API needs to have a route to delete cars from the database 
+5. The API needs to have a route to update a car in the database 
+6. The API should be able to accept a pageSize query param that limits the number of cars it should return. (An update to #1 in the workshop) 
+7. The API should be able to accept a brand queryParam that allows the user to get cars from that brand only. (Bonus points if you manage to make it work alongside #6) Don't worry about finishing everything today. 
